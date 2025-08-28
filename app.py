3c21f4d5444ca8b45b58bcff913894fb9cb5ed3d
i221158 <i221158@nu.edu.pk>
2025-08-28 11:11:43 -0700

    Introduce flake8 style violation

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 def about():
     return render_template("about.html")
 
-def bad_func():
+def    bad_func():
     print("this will fail flake8 test :()")
 if __name__ == "__main__":
     app.run(debug=True)
